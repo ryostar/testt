115836338ab48a9149bf4c600915e105d0cc9701
ryostar <82922436+ryostar@users.noreply.github.com>
2021-06-26 08:58:07 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -17,26 +17,8 @@ colors = [lg, r, w, cy, ye]
 try:
     import requests
 except ImportError:
-    print(f'{lg}[i] Installing module - requests...{n}')
+    print(f'{lg}[i] Cài đặt Modules - requests...{n}')
     os.system('pip install requests')
-
-def banner():
-    import random
-    # fancy logo
-    b = [
-    '88888888888  888b     d888 8888888888        d88P 888    d8P  888     888 8888888b.  8888888 .d8888b.   .d8888b.  ',
-    '    888      8888b   d8888 888              d88P  888   d8P   888     888 888   Y88b   888  d88P  Y88b d88P  Y88b ',
-    '    888      88888b.d88888 888             d88P   888  d8P    888     888 888    888   888  888        888    888 ',
-    '    888      888Y88888P888 8888888        d88P    888d88K     888     888 888   d88P   888  888d888b.  Y88b. d888 ',
-    '    888      888 Y888P 888 888           d88P     8888888b    888     888 8888888P"    888  888P "Y88b  "Y888P888 ',
-    '    888      888  Y8P  888 888          d88P      888  Y88b   888     888 888 T88b     888  888    888        888 ',
-    '    888  d8b 888   "   888 888         d88P       888   Y88b  Y88b. .d88P 888  T88b    888  Y88b  d88P Y88b  d88P ',
-    '    888  Y8P 888       888 8888888888 d88P        888    Y88b  "Y88888P"  888   T88b 8888888 "Y8888P"   "Y8888P"  '                                                                         
-    ]
-    for char in b:
-        print(f'{random.choice(colors)}{char}{n}')
-    print('=============SABO==============')
-    print(f'   Version: 1.0 | Tác giả: Sabo{n}\n')
 
 def clr():
     if os.name == 'nt':
